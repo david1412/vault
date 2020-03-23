f4a391e12d9d0a09efce3e19c4b1acb6142e04d7
Gopalam Moram <30881405+david1412@users.noreply.github.com>
2020-03-23 15:09:30 +0100

    Create variables.tf

---
+++ jx/modules/vault/variables.tf
@@ -0,0 +1,36 @@
+// ----------------------------------------------------------------------------
+// Required Variables
+// ----------------------------------------------------------------------------
+variable "gcp_project" {
+  description = "The name of the GCP project to create all resources"
+  type = string
+}
+
+variable "zone" {
+  description = "The GCloud zone in which to create the resources"
+  type = string
+}
+
+variable "cluster_name" {
+  description = "Name of the K8s cluster"
+  type = string
+}
+
+variable "cluster_id" {
+  description = "A random generated to uniqly name cluster resources"
+  type = string
+}
+
+variable "jenkins_x_namespace" {
+  description = "K8s namespace to install Jenkins X in"
+  type = string
+}
+
+// ----------------------------------------------------------------------------
+// Optional Variables
+// ----------------------------------------------------------------------------
+variable "force_destroy" {
+  description = "Flag to determine whether storage buckets get forcefully destroyed"
+  type        = bool
+  default     = false
+}
